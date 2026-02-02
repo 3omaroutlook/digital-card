9fd66bb3e33597b148ce7c3b83e6f8f1a3952e45
3omaroutlook <omarkamal.othman@outlook.com>
2026-02-02 05:17:58 +0200

    Update add-user.js

--- netlify/functions/add-user.js
+++ netlify/functions/add-user.js
@@ -1,27 +1,28 @@
-const { Client } = require('pg');
+import pkg from 'pg';
+const { Client } = pkg;
 
-exports.handler = async function(event, context) {
-    if(event.httpMethod !== 'POST') 
-        return { statusCode: 405, body: 'Method Not Allowed' };
+export async function handler(event, context) {
+  if(event.httpMethod !== 'POST') 
+    return { statusCode: 405, body: 'Method Not Allowed' };
 
-    const user = JSON.parse(event.body);
+  const user = JSON.parse(event.body);
 
-    const client = new Client({ connectionString: process.env.DATABASE_URL });
-    await client.connect();
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  await client.connect();
 
-    const query = `
-        INSERT INTO users(username,name,phone,email,instagram,whatsapp,image)
-        VALUES($1,$2,$3,$4,$5,$6,$7)
-        ON CONFLICT (username)
-        DO UPDATE SET name=$2,phone=$3,email=$4,instagram=$5,whatsapp=$6,image=$7
-    `;
+  const query = `
+    INSERT INTO users(username,name,phone,email,instagram,whatsapp,image)
+    VALUES($1,$2,$3,$4,$5,$6,$7)
+    ON CONFLICT (username)
+    DO UPDATE SET name=$2,phone=$3,email=$4,instagram=$5,whatsapp=$6,image=$7
+  `;
 
-    await client.query(query, [
-        user.username, user.name, user.phone, user.email, 
-        user.instagram, user.whatsapp, user.image
-    ]);
+  await client.query(query, [
+    user.username, user.name, user.phone, user.email,
+    user.instagram, user.whatsapp, user.image
+  ]);
 
-    await client.end();
+  await client.end();
 
-    return { statusCode: 200, body: JSON.stringify({message:'User added/updated successfully'}) };
+  return { statusCode: 200, body: JSON.stringify({message:'User added/updated successfully'}) };
 }
