012272c0405c91493216373580a74a2f985ea5a4
3omaroutlook <omarkamal.othman@outlook.com>
2026-02-02 05:18:23 +0200

    Update get-user.js

--- netlify/functions/get-user.js
+++ netlify/functions/get-user.js
@@ -1,18 +1,12 @@
-const { Client } = require('pg');
+import pkg from 'pg';
+const { Client } = pkg;
 
-exports.handler = async function(event, context) {
-    const params = event.queryStringParameters;
-    const username = params.user || 'omar';
+export async function handler(event, context) {
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  await client.connect();
 
-    const client = new Client({ connectionString: process.env.DATABASE_URL });
-    await client.connect();
+  const res = await client.query('SELECT * FROM users;');
+  await client.end();
 
-    const res = await client.query("SELECT * FROM users WHERE username=$1", [username]);
-    await client.end();
-
-    if(res.rows.length === 0) {
-        return { statusCode: 404, body: JSON.stringify({error:'User not found'}) };
-    }
-
-    return { statusCode: 200, body: JSON.stringify(res.rows[0]) };
+  return { statusCode: 200, body: JSON.stringify(res.rows) };
 }
